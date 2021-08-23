f94f970d1c5ab9ed87c15d8e11429194ac4bdd65
Janet Iqal <j.iqal35@gmail.com>
2021-08-23 16:19:09 -0500

    error fix

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -8,44 +8,45 @@ const { v4: uuidv4 } = require('uuid');
 router.get('/', (req, res) => res.json(data));
 
 // //POST SECTION
-router.post('/',  (req, res) => {
+router.post('/', (req, res) => {
     // Log that a POST request was received
     console.info(`${req.method} request received to add a note`);
-  
-  const { title, text } = req.body;
-  if (title && text ){
-    const newNote = {
-      title,
-      text,
-      note_id: uuidv4()
-    };
-    const noteString = JSON.stringify(newNote);
-    fs.readFile('../db/db.json',"utf8",(err, notesString)=>
-      {
-        if (err){
-          return res.send("Error w file system")
-        }
-        let notes = JSON.parse(notesString)
-        //pushing new notes to the array in db.json
-        notes.push(newNote);
-        console.log(notes)
-        fs.writeFile('../db/db.json', JSON.stringify(notes), (err) =>
-        err ? console.error(err): console.log(`${newNote.title} has been written to JSON file`));
-    })
 
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-  
-    console.log(response);
-     res.json(response);
-  } else {
-    res.json('Error in posting note');
-  }
-  });
-  
+    const { title, text, noteId } = req.body;
+    if (title && text && noteId) {
+        const newNote = {
+            title,
+            text,
+            noteId: uuidv4()
+        };
+        const noteString = JSON.stringify(newNote);
+        fs.readFile('../db/db.json', "utf8", (err, notesString) => {
+            if (err) {
+                console.log("We got an error in the readfile", err)
+            }
+            console.log("notes string" + notesString)
+            let notes = JSON.parse(notesString)
+            //pushing new notes to the array in db.json
+            notes.push(newNote);
+            console.log(notes)
+            fs.writeFile('../db/db.json', JSON.stringify(notes), (err) =>
+                err ? console.error(err) : console.log(`${newNote.title} has been written to JSON file`));
+        })
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.json(response);
+    } else {
+        res.json('Error in posting note');
+    }
+});
+
 //   //delete note
-//   // router.delete();
+// router.delete('/:note_id)', (res, req) => {
+    // const deleteNote= req.params.id
+// });
 
-  module.exports= router;
+module.exports = router;
